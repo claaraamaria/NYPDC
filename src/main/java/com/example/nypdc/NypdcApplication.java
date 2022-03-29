8bfcf6cb248cb1e86b08aa220ac3cb17f24d1ba6
claaraamaria <clrstancu@gmail.com>
2022-03-29 20:57:19 +0300

    Initial commit

---
+++ src/main/java/com/example/nypdc/NypdcApplication.java
@@ -0,0 +1,13 @@
+package com.example.nypdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NypdcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NypdcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nypdc/NypdcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nypdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NypdcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
